9a36de92b511ede8a79824aeaee0fbb065cec624
arman455 <snositel99@gmail.com>
2025-11-21 14:47:16 +0200

    feat: add husky pre-commit hook and update package dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,44 @@
-import express, { Express } from 'express';
-import cors from 'cors'
-import userRouter from './userApp/userRouter';
-import postRouter from './postApp/postRouter';
-import albumRouter from './albumApp/albumRouter';
-import chatsRouter from './Chats/chat.router';
-import path from 'path';
-import friendshipRouter from './friendshipApp/friendshipRouter';
-import { createServer } from 'http';
-import { initSocketServer } from './socket';
-import messageRouter from './Messages/message.router';
+import express, { Express } from "express";
+import cors from "cors";
+import userRouter from "./userApp/userRouter";
+import postRouter from "./postApp/postRouter";
+import albumRouter from "./albumApp/albumRouter";
+import chatsRouter from "./Chats/chat.router";
+import path from "path";
+import friendshipRouter from "./friendshipApp/friendshipRouter";
+import { createServer } from "http";
+import { initSocketServer } from "./socket";
+import messageRouter from "./Messages/message.router";
 
 const app: Express = express();
-const httpServer = createServer(app)
-initSocketServer(httpServer)
+const httpServer = createServer(app);
+initSocketServer(httpServer);
 
-const PORT = 3000
-const HOST = '192.168.0.254'
+const PORT = 3000;
+const HOST = "192.168.0.253";
 export const API_BASE_URL = `http://${HOST}:${PORT}`;
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(cors())
+app.use(cors());
 
-app.use('/user', userRouter);
+app.use("/users", userRouter);
 
-app.use('/posts', postRouter);
+app.use("/posts", postRouter);
 
-app.use('/albums', albumRouter);
+app.use("/albums", albumRouter);
 
-app.use('/uploads', express.static(path.join(__dirname, '..', 'public', 'uploads')));
+app.use(
+	"/uploads",
+	express.static(path.join(__dirname, "..", "public", "uploads"))
+);
 
-app.use("/friendship", friendshipRouter)
+app.use("/friendship", friendshipRouter);
 
-app.use("/chats", chatsRouter)
+app.use("/chats", chatsRouter);
 
-app.use("/messages", messageRouter)
-
+app.use("/messages", messageRouter);
 
 httpServer.listen(PORT, HOST, () => {
 	console.log(`server is running at http://${HOST}:${PORT}`);
