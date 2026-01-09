cc9e09227dc785a0004d5870664d732f55f9a899
arman455 <snositel99@gmail.com>
2026-01-09 16:28:45 +0200

    fix: add authTokenMiddleware to getChats route for improved security

--- src/сhatsApp/chatRouter.ts
+++ src/сhatsApp/chatRouter.ts
@@ -5,7 +5,7 @@ import { authTokenMiddleware } from "../middlewares/authTokenMiddleware";
 const router = express.Router();
 
 router.post("/create", authTokenMiddleware, chatController.createChat);
-router.get("/", chatController.getChats);
+router.get("/", authTokenMiddleware, chatController.getChats);
 router.get("/:id", chatController.getChat);
 router.delete("/:id", authTokenMiddleware, chatController.deleteChat);
 
